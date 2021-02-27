de3e6caaca780a8187a45c91dd17378f788e0d10
Mitch <79401825+TheRealMitch@users.noreply.github.com>
2021-02-27 17:59:19 +0200

    Add files via upload

---
+++ colors.py
@@ -0,0 +1,132 @@
+# By using this module you can play/change the color of your print() function
+#
+######
+# TheRealMitch
+######
+# Examples:
+# Print(Colors.Blink + Colors.Blink + "WARNING" + Colors.Full_Reset)
+######
+# Make sure to after using any color to Reset using Colors.Full_Reset
+######
+#
+
+
+
+class Colors:
+    """
+    To print colorfull string in terminal
+    """
+    # Reset
+    Full_Reset      = '\033[0m'
+    ResetBold       = "\033[21m"
+    ResetDim        = "\033[22m"
+    ResetUnderlined = "\033[24m"
+    ResetBlink      = "\033[25m"
+    ResetReverse    = "\033[27m"
+    ResetHidden     = "\033[28m"
+
+    # Features
+    Dim        = "\033[2m"
+    Underlined = "\033[4m"
+    Blink      = "\033[5m"
+    Reverse    = "\033[7m"
+    Hidden     = "\033[8m"
+
+    # Regular Colors
+    Default      = "\033[39m"
+    Black        = "\033[30m"
+    Red          = "\033[31m"
+    Green        = "\033[32m"
+    Yellow       = "\033[33m"
+    Blue         = "\033[34m"
+    Magenta      = "\033[35m"
+    Cyan         = "\033[36m"
+    LightGray    = "\033[37m"
+    DarkGray     = "\033[90m"
+    LightRed     = "\033[91m"
+    LightGreen   = "\033[92m"
+    LightYellow  = "\033[93m"
+    LightBlue    = "\033[94m"
+    LightMagenta = "\033[95m"
+    LightCyan    = "\033[96m"
+    White        = "\033[97m"
+
+    # Background
+    BackgroundDefault      = "\033[49m"
+    BackgroundBlack        = "\033[40m"
+    BackgroundRed          = "\033[41m"
+    BackgroundGreen        = "\033[42m"
+    BackgroundYellow       = "\033[43m"
+    BackgroundBlue         = "\033[44m"
+    BackgroundMagenta      = "\033[45m"
+    BackgroundCyan         = "\033[46m"
+    BackgroundLightGray    = "\033[47m"
+    BackgroundDarkGray     = "\033[100m"
+    BackgroundLightRed     = "\033[101m"
+    BackgroundLightGreen   = "\033[102m"
+    BackgroundLightYellow  = "\033[103m"
+    BackgroundLightBlue    = "\033[104m"
+    BackgroundLightMagenta = "\033[105m"
+    BackgroundLightCyan    = "\033[106m"
+    BackgroundWhite        = "\033[107m"
+
+    # Underline
+    UBlack        = "\033[4;30m"       
+    URed          = "\033[4;31m"       
+    UGreen        = "\033[4;32m"
+    UYellow       = "\033[4;33m" 
+    UBlue         = "\033[4;34m" 
+    UPurple       = "\033[4;35m"  
+    UCyan         = "\033[4;36m" 
+    UWhite        = "\033[4;37m" 
+    ULightMagenta = "\033[4;95m" 
+
+
+    # High Intensty
+    IBlack  = "\033[0;90m"
+    IRed    = "\033[0;91m"
+    IGreen  = "\033[0;92m"
+    IYellow = "\033[0;93m"
+    IBlue   = "\033[0;94m"
+    IPurple = "\033[0;95m"
+    ICyan   = "\033[0;96m"
+    IWhite  = "\033[0;97m"
+
+    # Bold High Intensty
+    BIBlack= "\033[1;90m"
+    BIRed=  "\033[1;91m"
+    BIGreen= "\033[1;92m"
+    BIYellow="\033[1;93m"
+    BIBlue=  "\033[1;94m"
+    BIPurple="\033[1;95m"
+    BICyan=  "\033[1;96m"
+    BIWhite= "\033[1;97m"
+    
+    # High Intensty backgrounds
+    BACK_HIBlack  = "\033[0;100m"
+    BACK_HIRed    = "\033[0;101m"
+    BACK_HIGreen  = "\033[0;102m"
+    BACK_HIYellow = "\033[0;103m"
+    BACK_HIBlue   = "\033[0;104m"
+    BACK_HIPurple = "\033[10;95m"
+    BACK_HICyan   = "\033[0;106m"
+    BACK_HIWhite  = "\033[0;107m"
+
+    # Dim
+    Default_Dim      = "\033[2;39m"
+    Black_Dim        = "\033[2;30m"
+    Red_Dim          = "\033[2;31m"
+    Green_Dim        = "\033[2;32m"
+    Yellow_Dim       = "\033[2;33m"
+    Blue_Dim         = "\033[2;34m"
+    Magenta_Dim      = "\033[2;35m"
+    Cyan_Dim         = "\033[2;36m"
+    LightGray_Dim    = "\033[2;37m"
+    DarkGray_Dim     = "\033[2;90m"
+    LightRed_Dim     = "\033[2;91m"
+    LightGreen_Dim   = "\033[2;92m"
+    LightYellow_Dim  = "\033[2;93m"
+    LightBlue_Dim    = "\033[2;94m"
+    LightMagenta_Dim = "\033[2;95m"
+    LightCyan_Dim    = "\033[2;96m"
+    White_Dim        = "\033[2;97m"
